9a1782f0a58c7ea8d8f8bd47bf2798d04bbaddc7
unknown <arunkumar98.ak95@gmail.com>
2020-06-24 15:27:07 +0530

    firebase authentication,email verifiaction , database storage done

--- src/Components/PageNotFound/PageNotFoundComponent.jsx
+++ src/Components/PageNotFound/PageNotFoundComponent.jsx
@@ -4,7 +4,7 @@ function PageNotFoundComponent() {
     return (
         <Fragment>
             <div className="container  justify-content-center">
-            <h1 style={{color:'red', fontSize:'50px',}}>Oops page not found d punda </h1> 
+            <h1 style={{color:'red', fontSize:'50px',}}>Oops page not found</h1> 
             <p style={{color:'red', fontSize:'50px',}}>404</p>
             </div>
         </Fragment>
